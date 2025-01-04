6a8a6ebfc43c55ddabae08077f2e91e451181724
zigler zhang <zigler.zhang@gmail.com>
2025-01-04 17:49:42 +0800

    =FIX: fix get syslink failed

--- custom_components/dnspod/ip_getter.py
+++ custom_components/dnspod/ip_getter.py
@@ -1,6 +1,8 @@
-from http import HTTPStatus
 import logging
+from http import HTTPStatus
+
 import requests
+
 from .const import DEFAULT_EXTERNAL_URLS, IP_REGEX
 
 _LOGGER = logging.getLogger(__name__)
@@ -10,11 +12,11 @@ def get_ip(conf):
     urls = []
     if conf:
         if "linksys" in conf:
-            ip = get_ip_from_linksys_router(conf['linksys'])
+            ip = get_ip_from_linksys_router(conf["linksys"])
             if ip:
                 _LOGGER.debug(f"get external ip {ip} from linksys")
                 return ip
-        urls = conf.get('external_urls', [])
+        urls = conf.get("external_urls", [])
     urls += DEFAULT_EXTERNAL_URLS
     for url in urls:
         ip = get_ip_from_website(url)
@@ -27,34 +29,31 @@ def get_ip(conf):
 
 def _make_linksys_request(url, action):
     # Weirdly enough, this doesn't seem to require authentication
-    data = [
-        {
-            "request": {},
-            "action": f"http://linksys.com/jnap/{action}"
-        }
-    ]
+    data = [{"request": {}, "action": f"http://linksys.com/jnap/{action}"}]
     headers = {
         "X-JNAP-Action": "http://linksys.com/jnap/core/Transaction",
-        'connection': 'close'
+        "connection": "close",
     }
     return requests.post(url, timeout=3, headers=headers, json=data)
 
 
 def get_ip_from_linksys_router(gw_ip):
     url = f"http://{gw_ip}/JNAP/"
-    r = _make_linksys_request(url, 'router/GetWANStatus')
-    if not r.status_code == HTTPStatus.OK:
+    r = _make_linksys_request(url, "router/GetWANStatus")
+    if r is None or not r.status_code == HTTPStatus.OK:
         _LOGGER.error("get ip failed: %s", r.text)
         return None
-    ip = r.json()\
-          .get('responses', [{}])[0]\
-          .get('output', {})['wanConnection']['ipAddress']
+    ip = (
+        r.json()
+        .get("responses", [{}])[0]
+        .get("output", {})["wanConnection"]["ipAddress"]
+    )
     return ip
 
 
 def get_ip_from_website(url):
     try:
-        r = requests.get(url=url, timeout=10, header={'connection': 'close'})
+        r = requests.get(url=url, timeout=10, header={"connection": "close"})
         return IP_REGEX.match(r.text).group(1)
     except Exception as e:
         _LOGGER.warning(f"get ip from {url} FAILED, error: {str(e)}")
